e8a0dbfc1a33eab9eae87da4c240dd3fd6b819e0
tempswamp <lauri.raitasuo@git.mail.kapsi.fi>
2022-12-04 17:36:38 +0200

    As per 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,27 +4,35 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
   
   const handleGood = () => {
     setGood(good + 1)
+    setAll(all + 1)
   }
 
   const handleNeutral = () => {
     setNeutral(neutral + 1)
+    setAll(all + 1)
   }
   const handleBad = () => {
     setBad(bad + 1)
+    setAll(all + 1)
   }
 
-  return (
+   return (
     <div>
       <h1>Give feedback</h1>
       <button onClick={handleGood}>Good</button>
       <button onClick={handleNeutral}>Good</button>
       <button onClick={handleBad}>Good</button>
       <h1>Stats</h1>
-      Good: {good} Neutral: {neutral} bad: {bad}
-      
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>All: {all}</p>
+      <p>Average: {(good - bad)/all}</p>
+      <p>Positive: {(good/all)*100} %</p>
     </div>
   )
 }
