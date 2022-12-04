6e9e5e0dd2171874fab4294a0a1a8adae5a5093f
tempswamp <lauri.raitasuo@git.mail.kapsi.fi>
2022-12-04 15:39:25 +0200

    Renamed folder

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,48 @@
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts}/>
+    </div>
+  )
+}
+
+const Header = (input) => {
+  return (<h1>{input.course}</h1>)
+}
+
+const Content = (input) => {
+  return(<div><Part part={input.parts[0].name} exercise={input.parts[0].exercises}/>
+  <Part part={input.parts[1].name} exercise={input.parts[1].exercises}/>
+  <Part part={input.parts[2].name} exercise={input.parts[2].exercises}/>
+  </div>)
+}
+
+const Part = (input) => {
+  return (<p>{input.part}: {input.exercise}</p>)
+}
+
+const Total = (input) => {
+  return (<div>Number of exercises: {input.parts[0].exercises + input.parts[1].exercises + input.parts[2].exercises}</div>)
+}
+
+export default App;
