cd91a5d0d62b2fb995ed0d1d67ffe74467888762
tempswamp <lauri.raitasuo@git.mail.kapsi.fi>
2022-12-04 18:44:22 +0200

    moved stuff around

---
+++ part1/anecdots/src/App.js
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+
+const Stats = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad  
+  if(all === 0)
+  {
+    return (<div>No feedback was given!</div>)
+  }
+  return(
+    <table>
+      <tbody><StatsLine input="Good" value={good}/>
+        <StatsLine input="Neutral" value={neutral}/>
+        <StatsLine input="Bad" value={bad}/>
+        <StatsLine input="All" value={good + neutral + bad} />
+        <StatsLine input="Average" value={(good - bad)/all} />
+        <StatsLine input="Positive" value={(good / all)*100 + " %"} />
+        </tbody>
+        </table>
+  )
+}
+
+const StatsLine = ({ input, value}) => (
+  <tr><td>{input}: </td>
+      <td>{value}</td></tr>
+)
+
+const Button = ({ handleClick, input }) => (
+  <button onClick={handleClick}>{input}</button>
+)
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+  const handleGood = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+  const handleBad = () => {
+    setBad(bad + 1)
+  }
+
+   return (
+    <div>
+      <h1>Give feedback</h1>
+      <Button handleClick={handleGood} input="Good" />
+      <Button handleClick={handleNeutral} input="Neutral" />
+      <Button handleClick={handleBad} input="Bad" />
+      <h1>Stats</h1>
+      <Stats good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+}
+
+export default App
