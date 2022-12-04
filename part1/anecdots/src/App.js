4e82053aab276a39c0d44778a7f019df660f253b
tempswamp <lauri.raitasuo@git.mail.kapsi.fi>
2022-12-04 19:09:22 +0200

    Per 1.12

--- part1/anecdots/src/App.js
+++ part1/anecdots/src/App.js
@@ -1,57 +1,21 @@
 import { useState } from 'react'
 
-const Stats = ({ good, neutral, bad }) => {
-  const all = good + neutral + bad  
-  if(all === 0)
-  {
-    return (<div>No feedback was given!</div>)
-  }
-  return(
-    <table>
-      <tbody><StatsLine input="Good" value={good}/>
-        <StatsLine input="Neutral" value={neutral}/>
-        <StatsLine input="Bad" value={bad}/>
-        <StatsLine input="All" value={good + neutral + bad} />
-        <StatsLine input="Average" value={(good - bad)/all} />
-        <StatsLine input="Positive" value={(good / all)*100 + " %"} />
-        </tbody>
-        </table>
-  )
-}
-
-const StatsLine = ({ input, value}) => (
-  <tr><td>{input}: </td>
-      <td>{value}</td></tr>
-)
-
-const Button = ({ handleClick, input }) => (
-  <button onClick={handleClick}>{input}</button>
-)
-
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  
-  const handleGood = () => {
-    setGood(good + 1)
-  }
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
 
-  const handleNeutral = () => {
-    setNeutral(neutral + 1)
-  }
-  const handleBad = () => {
-    setBad(bad + 1)
-  }
-
-   return (
+  return (
     <div>
-      <h1>Give feedback</h1>
-      <Button handleClick={handleGood} input="Good" />
-      <Button handleClick={handleNeutral} input="Neutral" />
-      <Button handleClick={handleBad} input="Bad" />
-      <h1>Stats</h1>
-      <Stats good={good} neutral={neutral} bad={bad}/>
+      {anecdotes[(Math.floor(Math.random() * 7))]}
     </div>
   )
 }
