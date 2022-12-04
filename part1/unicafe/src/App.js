97206f43acc3e85c548a91c64b97af605201b382
tempswamp <lauri.raitasuo@git.mail.kapsi.fi>
2022-12-04 16:39:25 +0200

    as per 1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,14 +1,30 @@
 import { useState } from 'react'
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  
+  const handleGood = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+  const handleBad = () => {
+    setBad(bad + 1)
+  }
 
   return (
     <div>
-      code here
+      <h1>Give feedback</h1>
+      <button onClick={handleGood}>Good</button>
+      <button onClick={handleNeutral}>Good</button>
+      <button onClick={handleBad}>Good</button>
+      <h1>Stats</h1>
+      Good: {good} Neutral: {neutral} bad: {bad}
+      
     </div>
   )
 }
